0eb516a7d05fbe6ff2e0b8b36d45037f021b38b4
wvicioso <wellington.vicioso@gmail.com>
2018-11-04 18:18:46 -0500

    Updates

---
+++ precache-manifest.7d4ff6fdd22a9b00fb5b1efff8fe48f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "45f9700a603df2409772",
+    "url": "/static/js/main.45f9700a.chunk.js"
+  },
+  {
+    "revision": "4737933a611d08b2018a",
+    "url": "/static/js/1.4737933a.chunk.js"
+  },
+  {
+    "revision": "45f9700a603df2409772",
+    "url": "/static/css/main.cf0721b4.chunk.css"
+  },
+  {
+    "revision": "2c0d8ddc1020eb6c0214aaca54699a5f",
+    "url": "/index.html"
+  }
+];
